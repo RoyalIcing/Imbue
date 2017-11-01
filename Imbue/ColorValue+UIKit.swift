be5c7ff6f8f94fc7eb1d9b3ea4fc95b079335b6b
Patrick Smith <patrick@burntcaramel.com>
2017-11-01 22:27:47 +1100

    Remove logging

--- Imbue/ColorValue+UIKit.swift
+++ Imbue/ColorValue+UIKit.swift
@@ -14,17 +14,13 @@ extension ColorValue {
 		guard let srgb = self.toSRGB()
 			else { return }
 		pb.string = srgb.hexString
-		print("Copied: \(pb.items)")
 	}
 	
 	init?(pasteboard pb: UIPasteboard) {
-		print("PASTE 1 hasColors: \(pb.hasColors) color: \(pb.color) colors: \(pb.colors) .items: \(pb.items) \(pb.string)")
-		
 		if
 			let string = pb.string,
 			let rgb = ColorValue.RGB(hexString: string)
 		{
-			print("PASTED \(rgb)")
 			self = .sRGB(rgb)
 			return
 		}
