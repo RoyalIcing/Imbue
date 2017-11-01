7c6dd3e86d83e1cf8e92064d5c3c8d2ef656dfef
Patrick Smith <patrick@burntcaramel.com>
2017-11-01 18:37:26 +1100

    Fix keyboard animation for iPhone X

--- Imbue/ViewConvenience.swift
+++ Imbue/ViewConvenience.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 
-extension UIViewController {
-	fileprivate func animateForKeyboardChange(constraint: NSLayoutConstraint, height: CGFloat, duration: TimeInterval, curve: UIViewAnimationCurve) {
-		constraint.constant = height
-		view.setNeedsUpdateConstraints()
+func heightIntersecting(keyboardFrame: CGRect, withView view: UIView, layoutGuide: UILayoutGuide) -> CGFloat {
+	let keyboardFrameInView = view.convert(keyboardFrame, from: nil)
+	let intersection = layoutGuide.layoutFrame.intersection(keyboardFrameInView)
+	return intersection.height
+}
+
+extension UIView {
+	func animateFor(keyboardNotification: Notification, useKeyboardEndFrame: (CGRect) -> ()) {
+		guard
+			let info = keyboardNotification.userInfo,
+			let keyboardFrame = info[UIKeyboardFrameEndUserInfoKey] as? CGRect,
+			let duration = info[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval,
+			let animationCurveInt = info[UIKeyboardAnimationCurveUserInfoKey] as? Int, //UIViewAnimationCurve
+			let animationCurve = UIViewAnimationCurve(rawValue: animationCurveInt)
+			else { return }
+		
+		useKeyboardEndFrame(keyboardFrame)
 		
 		UIView.beginAnimations("keyboard", context: nil)
 		UIView.setAnimationDuration(duration)
-		UIView.setAnimationCurve(curve)
-		view.layoutIfNeeded()
+		UIView.setAnimationCurve(animationCurve)
+		self.layoutIfNeeded()
 		UIView.commitAnimations()
 	}
 }
@@ -28,26 +41,32 @@ enum ViewConvenience {
 		return [
 			nc.addObserver(forName: .UIKeyboardWillShow, object: nil, queue: nil) { [weak viewController] note in
 				guard let viewController = viewController else { return }
-				guard
-					let info = note.userInfo,
-					let keyboardRect = info[UIKeyboardFrameEndUserInfoKey] as? CGRect,
-					let duration = info[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval,
-					let animationCurveInt = info[UIKeyboardAnimationCurveUserInfoKey] as? Int, //UIViewAnimationCurve
-					let animationCurve = UIViewAnimationCurve(rawValue: animationCurveInt)
-					else { return }
-				
-				viewController.animateForKeyboardChange(constraint: constraint, height: keyboardRect.height, duration: duration, curve: animationCurve)
+				let view = viewController.view!
+				view.animateFor(keyboardNotification: note, useKeyboardEndFrame: { keyboardFrame in
+					if #available(iOS 11.0, *) {
+						let insetHeight = heightIntersecting(keyboardFrame: keyboardFrame, withView: view, layoutGuide: view.safeAreaLayoutGuide)
+						
+						viewController.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: insetHeight, right: 0)
+					}
+					else {
+						constraint.constant = keyboardFrame.height
+						view.setNeedsUpdateConstraints()
+					}
+				})
 			},
 			nc.addObserver(forName: .UIKeyboardWillHide, object: nil, queue: nil) { [weak viewController] note in
 				guard let viewController = viewController else { return }
-				guard
-					let info = note.userInfo,
-					let duration = info[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval,
-					let animationCurveInt = info[UIKeyboardAnimationCurveUserInfoKey] as? Int, //UIViewAnimationCurve
-					let animationCurve = UIViewAnimationCurve(rawValue: animationCurveInt)
-					else { return }
+				let view = viewController.view!
 				
-				viewController.animateForKeyboardChange(constraint: constraint, height: valueWhenHidden, duration: duration, curve: animationCurve)
+				view.animateFor(keyboardNotification: note, useKeyboardEndFrame: { keyboardFrame in
+					if #available(iOS 11.0, *) {
+						viewController.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+					}
+					else {
+						constraint.constant = valueWhenHidden
+						view.setNeedsUpdateConstraints()
+					}
+				})
 			}
 		]
 	}
